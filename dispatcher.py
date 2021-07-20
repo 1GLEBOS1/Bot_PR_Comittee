8a6d0db09bcbaa55b3357d5bc7cdd373c76354a3
1GLEBOS1 <glebxx04@gmail.com>
2021-07-20 23:16:17 +0300

    Adds custom filters to dispatcher

--- dispatcher.py
+++ dispatcher.py
@@ -5,3 +5,9 @@ from filter import IsOwnerFilter, IsChairmanFilter, IsPRComitteeMemberFilter, Is
 # Inits
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
+
+#
+dp.filters_factory.resolve(IsOwnerFilter, event_handler=[dp.message_handler])
+dp.filters_factory.resolve(IsChairmanFilter, event_handler=[dp.message_handler])
+dp.filters_factory.resolve(IsPRComitteeMemberFilter, event_handler=[dp.message_handler])
+dp.filters_factory.resolve(IsOwnerOrChairmanFilter, event_handler=[dp.message_handler])
