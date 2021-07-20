4a7422ed85e0bd7dfa68b55aa3f7cf979d3b12b5
1GLEBOS1 <glebxx04@gmail.com>
2021-07-20 23:32:04 +0300

    Added event_loop value to filters

--- dispatcher.py
+++ dispatcher.py
@@ -7,7 +7,7 @@ bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
 #
-dp.filters_factory.resolve(IsOwnerFilter, event_handler=[dp.message_handler])
-dp.filters_factory.resolve(IsChairmanFilter, event_handler=[dp.message_handler])
-dp.filters_factory.resolve(IsPRComitteeMemberFilter, event_handler=[dp.message_handler])
-dp.filters_factory.resolve(IsOwnerOrChairmanFilter, event_handler=[dp.message_handler])
+dp.filters_factory.resolve(IsOwnerFilter, dp.message_handler())
+dp.filters_factory.resolve(IsChairmanFilter, dp.message_handler())
+dp.filters_factory.resolve(IsPRComitteeMemberFilter, dp.message_handler())
+dp.filters_factory.resolve(IsOwnerOrChairmanFilter, dp.message_handler())
