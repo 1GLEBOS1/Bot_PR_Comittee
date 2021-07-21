1cd9c74523a56925ba9efa6f45c7f74f086475e5
1GLEBOS1 <glebxx04@gmail.com>
2021-07-21 15:19:14 +0300

    Fixed activation filters

--- dispatcher.py
+++ dispatcher.py
@@ -1,13 +1,12 @@
 from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
-from filter import IsOwnerFilter, IsChairmanFilter, IsPRComitteeMemberFilter, IsOwnerOrChairmanFilter
+from filter import IsOwnerFilter, IsChairmanFilter, IsPRComitteeMemberFilter
 
 # Inits
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
-#
-dp.filters_factory.resolve(IsOwnerFilter, dp.message_handler())
-dp.filters_factory.resolve(IsChairmanFilter, dp.message_handler())
-dp.filters_factory.resolve(IsPRComitteeMemberFilter, dp.message_handler())
-dp.filters_factory.resolve(IsOwnerOrChairmanFilter, dp.message_handler())
+# Activation filters
+dp.filters_factory.bind(IsOwnerFilter)
+dp.filters_factory.bind(IsChairmanFilter)
+dp.filters_factory.bind(IsPRComitteeMemberFilter)
