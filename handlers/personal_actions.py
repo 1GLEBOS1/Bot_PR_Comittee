1bb67c35a3fe85de7c12a69e42dee1ff50e39abb
1GLEBOS1 <glebxx04@gmail.com>
2021-07-21 15:55:44 +0300

    Added /add_member with FSM

--- handlers/personal_actions.py
+++ handlers/personal_actions.py
@@ -1,6 +1,7 @@
 from aiogram import types
+from aiogram.dispatcher.storage import FSMContext
 from dispatcher import dp
-from filter import IsOwnerFilter, IsChairmanFilter, IsPRComitteeMemberFilter
+from finete_state_machine import AddMember
 
 
 @dp.message_handler(commands=['start'])
@@ -12,3 +13,29 @@ async def start(message=types.Message):
 @dp.message_handler(is_chairman=True, commands=['help'])
 async def help_(message=types.Message):
     await message.reply(text='', reply=False)
+
+
+@dp.message_handler(is_owner=True, commands=['add_member'])
+@dp.message_handler(is_chairman=True, commands=['add_member'])
+async def get_telegram_id(message=types.Message):
+    pass
+
+
+@dp.message_handler(state=AddMember.get_telegram_id)
+async def get_name(message=types.Message, state=FSMContext):
+    pass
+
+
+@dp.message_handler(state=AddMember.get_name)
+async def get_access_level(message=types.Message, state=FSMContext):
+    pass
+
+
+@dp.message_handler(state=AddMember.get_access_level)
+async def get_name(message=types.Message, state=FSMContext):
+    pass
+
+
+@dp.message_handler(state=AddMember.get_position)
+async def get_access_level(message=types.Message, state=FSMContext):
+    pass
