3cf9f2727bc9cdb859d9bbc5260532d9866b9bd4
1GLEBOS1 <glebxx04@gmail.com>
2021-07-21 22:18:04 +0300

    Added /create_db functional

--- handlers/personal_actions.py
+++ handlers/personal_actions.py
@@ -1,25 +1,27 @@
+import peewee
 from aiogram import types
 from aiogram.dispatcher.storage import FSMContext
 from dispatcher import dp, bot
 from finete_state_machine import AddMember, ViewStatistic, AddStatistic
+from database.interface import InterfaceStatistic, InterfacePRCommitteeMember
 
 
 # /start
 @dp.message_handler(commands=['start'])
 async def start(message=types.Message):
-    await message.reply(text=f'Ваш telegram id: {message.from_user.id}', reply=False)
+    await message.answer(text=f'Ваш telegram id: {message.from_user.id}')
 
 
 # /help
 @dp.message_handler(commands=['help'])
 async def help_(message=types.Message):
-    await message.reply(text='', reply=False)
+    await message.answer(text='Заглушка')
 
 
 @dp.message_handler(commands=['debug'], state='*')
 async def cancel(message=types.Message, state=FSMContext):
     current_state = await state.get_state()
-    await message.reply(text='Машина состояний сброшена, наши программисты уже работают над исправлением ошибки')
+    await message.answer(text='Машина состояний сброшена, наши программисты уже работают над исправлением ошибки')
     await bot.send_message(chat_id=870069981, text=f'Username: {message.from_user.username}, chat_id: {message.chat.id}'
                                                    f', state: {current_state}')
     await state.finish()
@@ -28,7 +30,12 @@ async def cancel(message=types.Message, state=FSMContext):
 # /created_db
 @dp.message_handler(is_owner=True, commands=['create_db'])
 async def createdb(message=types.Message):
-    pass
+    try:
+        InterfaceStatistic.create_db()
+        InterfacePRCommitteeMember.create_db()
+        await message.answer(text='Базы данных созданы')
+    except peewee.DatabaseError as e:
+        await message.answer(text=f'Ошибка: {e}')
 
 
 # /add_member
