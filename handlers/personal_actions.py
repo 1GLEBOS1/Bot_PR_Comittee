da565b7124f4ba1d705c197c85b589bb49b8da44
1GLEBOS1 <glebxx04@gmail.com>
2021-07-21 15:21:53 +0300

    Added /help handler with filters and changed /start message

--- handlers/personal_actions.py
+++ handlers/personal_actions.py
@@ -1,7 +1,14 @@
 from aiogram import types
 from dispatcher import dp
+from filter import IsOwnerFilter, IsChairmanFilter, IsPRComitteeMemberFilter
 
 
 @dp.message_handler(commands=['start'])
 async def start(message=types.Message):
-    await message.reply(text=message.from_user.id)
+    await message.reply(text=f'Ваш telegram id: {message.from_user.id}', reply=False)
+
+
+@dp.message_handler(is_owner=True, commands=['help'])
+@dp.message_handler(is_chairman=True, commands=['help'])
+async def help_(message=types.Message):
+    await message.reply(text='', reply=False)
