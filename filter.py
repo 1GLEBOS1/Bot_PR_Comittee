4a53dd1dbd8cede02890f96451f8c7fb4b4af15c
1GLEBOS1 <glebxx04@gmail.com>
2021-07-22 07:54:13 +0300

    Changed filters to handle DoesNotExists exception

--- filter.py
+++ filter.py
@@ -1,5 +1,7 @@
+from peewee import DoesNotExist
 from aiogram.dispatcher.filters.filters import BoundFilter
 from aiogram import types
+from database.interface import InterfacePRCommitteeMember
 
 
 class IsOwnerFilter(BoundFilter):
@@ -13,7 +15,11 @@ class IsOwnerFilter(BoundFilter):
         self.is_owner = is_owner
 
     async def check(self, message: types.Message):
-        return message.from_user.id == 870069981
+        try:
+            InterfacePRCommitteeMember.get_owner_id()
+            return message.from_user.id == InterfacePRCommitteeMember.get_owner_id()
+        except DoesNotExist:
+            return False
 
 
 class IsChairmanFilter(BoundFilter):
@@ -27,7 +33,11 @@ class IsChairmanFilter(BoundFilter):
         self.is_chairman = is_chairman
 
     async def check(self, message: types.Message):
-        return False
+        try:
+            InterfacePRCommitteeMember.get_chairman_id()
+            return message.from_user.id == InterfacePRCommitteeMember.get_chairman_id()
+        except DoesNotExist:
+            return False
 
 
 class IsPRComitteeMemberFilter(BoundFilter):
@@ -41,4 +51,4 @@ class IsPRComitteeMemberFilter(BoundFilter):
         self.is_pr_comittee_member = is_pr_comittee_member
 
     async def check(self, message: types.Message):
-        pass
+        return message.from_user.id in InterfacePRCommitteeMember.get_pr_mambers_id()
