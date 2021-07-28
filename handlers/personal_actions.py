768b94c5ce937a659d03cb93c489aa00ddd33eb9
1GLEBOS1 <glebxx04@gmail.com>
2021-07-28 23:14:15 +0300

    Refactoring

--- handlers/personal_actions.py
+++ handlers/personal_actions.py
@@ -8,67 +8,67 @@ from statistic_analyser.analyser import Analyser
 
 
 # /start
-@dp.message_handler(commands=['start'])
+@dp.message_handler(commands=["start"])
 async def start(message=types.Message):
-    await message.answer(text=f'Ваш telegram id: {message.from_user.id}')
+    await message.answer(text=f"Ваш telegram id: {message.from_user.id}")
 
 
 # /help
-@dp.message_handler(commands=['help'])
+@dp.message_handler(commands=["help"])
 async def help_(message=types.Message):
-    await message.answer(text='Это бот для внутреннего использования Комитета ПИАРа Ученического совета г.Краснодар.\n'
-                              'Этот бот умеет собирать статистику по пиару мероприятий.\n'
-                              'Если у Вас есть вопросы, пишите их @Gleb_Polyakov')
+    await message.answer(text="Это бот для внутреннего использования Комитета ПИАРа Ученического совета г.Краснодар.\n"
+                              "Этот бот умеет собирать статистику по пиару мероприятий.\n"
+                              "Если у Вас есть вопросы, пишите их @Gleb_Polyakov")
 
 
 # /debug
-@dp.message_handler(commands=['debug'], state='*')
+@dp.message_handler(commands=["debug"], state="*")
 async def cancel(message=types.Message, state=FSMContext):
     current_state = await state.get_state()
-    await message.answer(text='Машина состояний сброшена, наши программисты уже работают над исправлением ошибки')
-    await bot.send_message(chat_id=-554348036, text=f'Username: {message.from_user.username}, '
-                                                    f'chat_id: {message.chat.id}, '
-                                                    f'state: {current_state}')
+    await message.answer(text="Машина состояний сброшена, наши программисты уже работают над исправлением ошибки")
+    await bot.send_message(chat_id=-554348036, text=f"Username: {message.from_user.username}, "
+                                                    f"chat_id: {message.chat.id}, "
+                                                    f"state: {current_state}")
     await state.finish()
 
 
 # /created_db
-@dp.message_handler(is_owner=True, commands=['create_db'])
+@dp.message_handler(is_owner=True, commands=["create_db"])
 async def createdb(message=types.Message):
     try:
         InterfaceStatistic.create_db()
         InterfacePRCommitteeMember.create_db()
-        await message.answer(text='Базы данных созданы')
+        await message.answer(text="Базы данных созданы")
     except peewee.DatabaseError as e:
-        await message.answer(text=f'Ошибка: {e}')
-        await bot.send_message(chat_id=870069981, text=f'Username: {message.from_user.username}, '
-                                                       f'chat_id: {message.chat.id}'
-                                                       f', error: {e}')
+        await message.answer(text=f"Ошибка: {e}")
+        await bot.send_message(chat_id=870069981, text=f"Username: {message.from_user.username}, "
+                                                       f"chat_id: {message.chat.id}"
+                                                       f", error: {e}")
 
 
 # /add_member
-@dp.message_handler(is_owner=True, commands=['add_member'])
-@dp.message_handler(is_chairman=True, commands=['add_member'])
+@dp.message_handler(is_owner=True, commands=["add_member"])
+@dp.message_handler(is_chairman=True, commands=["add_member"])
 async def get_telegram_id(message=types.Message):
     await AddMember.first()
-    await message.answer(text='Введите telegram id:')
+    await message.answer(text="Введите telegram id:")
 
 
 @dp.message_handler(state=AddMember.get_telegram_id)
 async def get_name(message=types.Message, state=FSMContext):
     try:
         await state.update_data(telegram_id=int(message.text))
-        await message.answer(text='Введите имя:')
+        await message.answer(text="Введите имя:")
         await AddMember.next()
     except ValueError:
-        await message.answer(text='Вводите telegram id цифрами')
+        await message.answer(text="Вводите telegram id цифрами")
         await state.finish()
 
 
 @dp.message_handler(state=AddMember.get_name)
 async def get_access_level(message=types.Message, state=FSMContext):
     await state.update_data(name=message.text)
-    await message.answer(text='Введите уровень доступа:')
+    await message.answer(text="Введите уровень доступа:")
     await AddMember.next()
 
 
@@ -76,10 +76,10 @@ async def get_access_level(message=types.Message, state=FSMContext):
 async def get_position(message=types.Message, state=FSMContext):
     try:
         await state.update_data(access_level=int(message.text))
-        await message.answer(text='Введите позицию:')
+        await message.answer(text="Введите позицию:")
         await AddMember.next()
     except ValueError:
-        await message.answer(text='Вводите уровень доступа цифрами')
+        await message.answer(text="Вводите уровень доступа цифрами")
         await state.finish()
 
 
@@ -89,17 +89,17 @@ async def add_member(message=types.Message, state=FSMContext):
     person_data = await state.get_data()
     try:
         InterfacePRCommitteeMember.add_member(
-            telegram_id=person_data['telegram_id'],
-            name=person_data['name'],
-            access_level=person_data['access_level'],
-            position=person_data['position']
+            telegram_id=person_data["telegram_id"],
+            name=person_data["name"],
+            access_level=person_data["access_level"],
+            position=person_data["position"]
         )
-        await message.answer(text='Успешно')
+        await message.answer(text="Успешно")
     except peewee.DatabaseError as e:
-        await message.answer(f'Ошибка: {e}')
+        await message.answer(f"Ошибка: {e}")
         await bot.send_message(chat_id=870069981,
-                                text=f'Username: {message.from_user.username}, chat_id: {message.chat.id}'
-                                    f', error: {e}'
+                                text=f"Username: {message.from_user.username}, chat_id: {message.chat.id}"
+                                    f", error: {e}"
                                 )
 
     finally:
@@ -107,10 +107,10 @@ async def add_member(message=types.Message, state=FSMContext):
 
 
 # /view_statistic
-@dp.message_handler(is_chairman=True, commands=['view_statistic'])
-@dp.message_handler(is_owner=True, commands=['view_statistic'])
+@dp.message_handler(is_chairman=True, commands=["view_statistic"])
+@dp.message_handler(is_owner=True, commands=["view_statistic"])
 async def view_get_event_id(message=types.Message):
-    await message.answer(text='Введите id мероприятия')
+    await message.answer(text="Введите id мероприятия")
     await ViewStatistic.first()
 
 
@@ -123,29 +123,29 @@ async def view_add_statistic(message=types.Message, state=FSMContext):
         await message.answer(text=a.get_stats(event_id=get_data["event_id"]))
         del a
     except ValueError:
-        await message.answer(text='Вводите id мероприятия цифрами')
+        await message.answer(text="Вводите id мероприятия цифрами")
     finally:
         await state.finish()
 
 
 # /add_statistic
-@dp.message_handler(is_pr_comittee_member=True, commands=['add_statistic'])
-@dp.message_handler(is_chairman=True, commands=['add_statistic'])
-@dp.message_handler(is_owner=True, commands=['add_statistic'])
+@dp.message_handler(is_pr_comittee_member=True, commands=["add_statistic"])
+@dp.message_handler(is_chairman=True, commands=["add_statistic"])
+@dp.message_handler(is_owner=True, commands=["add_statistic"])
 async def get_event_id(message=types.Message, state=FSMContext):
     await AddStatistic.first()
     await state.update_data(author_id=message.from_user.id)
-    await message.answer(text='Введите id мероприятия')
+    await message.answer(text="Введите id мероприятия")
 
 
 @dp.message_handler(state=AddStatistic.get_statistic)
 async def get_statistic(message=types.Message, state=FSMContext):
     try:
         await state.update_data(event_id=int(message.text))
-        await message.answer(text='Введите статистику')
+        await message.answer(text="Введите статистику")
         await AddStatistic.next()
     except ValueError:
-        await message.answer(text='Вводите id мероприятия цифрами')
+        await message.answer(text="Вводите id мероприятия цифрами")
         await state.finish()
 
 
@@ -154,13 +154,13 @@ async def add_statistic_to_database(message=types.Message, state=FSMContext):
     await state.update_data(statistic=message.text)
     statistic_data = await state.get_data()
     try:
-        InterfaceStatistic.add_statistic(statistic=statistic_data['statistic'], author_id=statistic_data['author_id'],
-                                         event_id=statistic_data['event_id'])
-        await message.answer('Успешно')
+        InterfaceStatistic.add_statistic(statistic=statistic_data["statistic"], author_id=statistic_data["author_id"],
+                                         event_id=statistic_data["event_id"])
+        await message.answer("Успешно")
     except peewee.DatabaseError as e:
-        await message.answer(f'Ошибка: {e}')
+        await message.answer(f"Ошибка: {e}")
         await bot.send_message(chat_id=870069981,
-                               text=f'Username: {message.from_user.username}, chat_id: {message.chat.id}'
-                                    f', error: {e}')
+                               text=f"Username: {message.from_user.username}, chat_id: {message.chat.id}"
+                                    f", error: {e}")
     finally:
         await state.finish()
